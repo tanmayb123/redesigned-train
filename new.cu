fcd768e793c69dded660a5db42e579a0bbd6fd83
Tanmay Bakshi <tajymany@gmail.com>
2020-06-14 03:03:44 +0000

    intense optimizations

--- new.cu
+++ new.cu
@@ -4,38 +4,20 @@
 #include <cuda.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <locale.h>
 #include "sha256.cuh"
 
 #define TEXT "Tanmay Bakshi"
 #define TEXT_LEN 13
 #define BLOCK_SIZE 400000
 #define GPUS 4
-#define DIFFICULTY 4
+#define DIFFICULTY 5
 #define RANDOM_LEN 20
 
 __constant__ BYTE characterSet[63] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
 
-__global__ void sha256_cuda(BYTE *prefix, BYTE *randoms, BYTE *solves) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < BLOCK_SIZE) {
-        SHA256_CTX ctx;
-        BYTE digest[32];
-        sha256_init(&ctx);
-        sha256_update(&ctx, prefix, TEXT_LEN);
-        sha256_update(&ctx, randoms + i * RANDOM_LEN, RANDOM_LEN);
-        sha256_final(&ctx, digest);
-        for (int j = 0; j < DIFFICULTY; j++) {
-            if (digest[j] > 0) {
-                solves[i] = 0;
-                return;
-            }
-        }
-/*        if ((digest[4] & 0xC0) > 0) {
-            solves[i] = 0;
-            return;
-        }*/
-        solves[i] = 1;
-    }
+__global__ void initSolutionMemory(int *blockContainsSolution) {
+    *blockContainsSolution = -1;
 }
 
 __device__ void deviceRandomGen(unsigned long *x) {
@@ -44,23 +26,40 @@ __device__ void deviceRandomGen(unsigned long *x) {
     *x ^= (*x << 4);
 }
 
-void hostRandomGen(unsigned long *x) {
-    *x ^= (*x << 21);
-    *x ^= (*x >> 35);
-    *x ^= (*x << 4);
-}
-
-__global__ void generateStrings(BYTE *block, unsigned long baseSeed) {
+__global__ void sha256_cuda(BYTE *prefix, BYTE *solution, int *blockContainsSolution, unsigned long baseSeed) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < BLOCK_SIZE) {
+        SHA256_CTX ctx;
+        BYTE digest[32];
+        BYTE random[20];
         unsigned long seed = baseSeed;
         seed += (unsigned long) i;
         for (int j = 0; j < RANDOM_LEN; j++) {
             deviceRandomGen(&seed);
             int randomIdx = (int) (seed % 62);
-            (block + i * RANDOM_LEN)[j] = characterSet[randomIdx];
+            random[j] = characterSet[randomIdx];
         }
+        sha256_init(&ctx);
+        sha256_update(&ctx, prefix, TEXT_LEN);
+        sha256_update(&ctx, random, RANDOM_LEN);
+        sha256_final(&ctx, digest);
+        for (int j = 0; j < DIFFICULTY; j++) {
+            if (digest[j] > 0) {
+                return;
+            }
+        }
+        if (*blockContainsSolution == 1)
+            return;
+        *blockContainsSolution = 1;
+        for (int j = 0; j < RANDOM_LEN; j++)
+            solution[j] = random[j];
     }
+}
+
+void hostRandomGen(unsigned long *x) {
+    *x ^= (*x << 21);
+    *x ^= (*x >> 35);
+    *x ^= (*x << 4);
 }
 
 void pre_sha256() {
@@ -94,36 +93,33 @@ void *launchGPUHandlerThread(void *vargp) {
     cudaMalloc(&d_prefix, TEXT_LEN);
     cudaMemcpy(d_prefix, cpuPrefix, TEXT_LEN, cudaMemcpyHostToDevice);
 
-    BYTE *rndBlock = (BYTE *) calloc(sizeof(BYTE), BLOCK_SIZE * RANDOM_LEN);
-    BYTE *d_rndBlock;
-    cudaMalloc(&d_rndBlock, sizeof(BYTE) * BLOCK_SIZE * RANDOM_LEN);
+    BYTE *blockSolution = (BYTE *) malloc(sizeof(BYTE) * RANDOM_LEN);
+    BYTE *d_solution;
+    cudaMalloc(&d_solution, sizeof(BYTE) * RANDOM_LEN);
 
-    BYTE *solves = (BYTE *) calloc(sizeof(BYTE), BLOCK_SIZE);
-    BYTE *d_solves;
-    cudaMalloc(&d_solves, sizeof(BYTE) * BLOCK_SIZE);
+    int *blockContainsSolution = (int *) malloc(sizeof(int));
+    int *d_blockContainsSolution;
+    cudaMalloc(&d_blockContainsSolution, sizeof(int));
 
     unsigned long rngSeed = timems();
     hostRandomGen(&rngSeed);
 
+    initSolutionMemory<<<1, 1>>>(d_blockContainsSolution);
+
     while (1) {
-        generateStrings<<<BLOCK_SIZE / 256, 256>>>(d_rndBlock, rngSeed);
         hostRandomGen(&rngSeed);
-        cudaDeviceSynchronize();
 
         hi->hashesProcessed += BLOCK_SIZE;
-        sha256_cuda<<<BLOCK_SIZE / 256, 256>>>(d_prefix, d_rndBlock, d_solves);
+        sha256_cuda<<<BLOCK_SIZE / 256, 256>>>(d_prefix, d_solution, d_blockContainsSolution, rngSeed);
         cudaDeviceSynchronize();
 
-        cudaMemcpy(rndBlock, d_rndBlock, sizeof(BYTE) * BLOCK_SIZE * RANDOM_LEN, cudaMemcpyDeviceToHost);
-        cudaMemcpy(solves, d_solves, sizeof(BYTE) * BLOCK_SIZE, cudaMemcpyDeviceToHost);
-
-        for (int i = 0; i < BLOCK_SIZE; i++) {
-            if (solves[i] == 1) {
-                pthread_mutex_lock(&provideSolutionLock);
-                solution = rndBlock + i * RANDOM_LEN;
-                pthread_mutex_unlock(&solutionLock);
-                goto finish;
-            }
+        cudaMemcpy(blockContainsSolution, d_blockContainsSolution, sizeof(int), cudaMemcpyDeviceToHost);
+        if (*blockContainsSolution == 1) {
+            cudaMemcpy(blockSolution, d_solution, sizeof(BYTE) * RANDOM_LEN, cudaMemcpyDeviceToHost);
+            pthread_mutex_lock(&provideSolutionLock);
+            solution = blockSolution;
+            pthread_mutex_unlock(&solutionLock);
+            goto finish;
         }
     }
 
@@ -137,7 +133,6 @@ int main() {
 
     pthread_mutex_lock(&solutionLock);
 
-    pthread_t *tids = (pthread_t *) malloc(sizeof(pthread_t) * GPUS);
     unsigned long **processedPtrs = (unsigned long **) malloc(sizeof(unsigned long *) * GPUS);
     long long start = timems();
     for (int i = 0; i < GPUS; i++) {
@@ -147,7 +142,6 @@ int main() {
         processedPtrs[i] = &hi->hashesProcessed;
         pthread_t tid;
         pthread_create(&tid, NULL, launchGPUHandlerThread, hi);
-        tids[i] = tid;
         usleep(10);
     }
 
@@ -158,12 +152,14 @@ int main() {
     unsigned long totalProcessed = 0;
     for (int i = 0; i < GPUS; i++) {
         totalProcessed += *(processedPtrs[i]);
-        pthread_cancel(tids[i]);
     }
+    setlocale(LC_NUMERIC, "");
     printf("Solution: %.20s\n", solution);
-    printf("Hashes processed: %lu\n", totalProcessed);
+    printf("Hashes processed: %'lu\n", totalProcessed);
     printf("Time: %llu\n", elapsed);
-    printf("Hashes/sec: %f\n", ((float) totalProcessed / (float) elapsed) * 1000);
+    printf("Hashes/sec: %'lu\n", (unsigned long) ((double) totalProcessed / (double) elapsed) * 1000);
+
+    cudaDeviceReset();
 
     return 0;
 }
